ae993dfc0af3269d04c3de346e8d191adef5cedb
elisa <elisa@LAPTOP-LUBLRQT2>
2023-03-06 10:42:25 +0100

    Mi primer commit ElisaPalmaPacheco2223

--- src/vehiculo/Main.java
+++ src/vehiculo/Main.java
@@ -1,12 +1,21 @@
-
 package vehiculo;
-
 /**
- *
+ * Clase main del programa, que define un objeto Vehículo con unos parámetros y realiza
+ * acciones de vender y comprar vehículos
+ * 
  * @author Elisa Palma Pacheco
  */
 public class Main {
+    /**
+* Método main.
+*
+* Este método es el principal de ejecución del programa
+*
+*/
     
+     /**
+     * @param args the command line arguments
+     */
       public static void main(String[] args) {
         VehiculoPalmaPachecoElisa2223 miVehiculoPalmaPachecoElisa2223;
         int stockActual;
@@ -14,7 +23,7 @@ public class Main {
         miVehiculoPalmaPachecoElisa2223 = new VehiculoPalmaPachecoElisa2223("Seat",18000,100);
         operativaVehiculosPalmaPachecoElisa2223(miVehiculoPalmaPachecoElisa2223, 50); 
     }
-
+ /*Vamos a vender vehículos, como hay suficientes vehículos se podrán vender */
     public static void operativaVehiculosPalmaPachecoElisa2223(VehiculoPalmaPachecoElisa2223 miVehiculoPalmaPachecoElisa2223, int cantidad) {
         int stockActual;
         try
@@ -25,6 +34,7 @@ public class Main {
         {
             System.out.print("Fallo al vender");
         }
+         /*Vamos a comprar más vehículos, ya que con la venta ha bajado el stock. Una vez compremos los vehículos se añadirán al stock*/
         try
         {
             System.out.println("Compra de Vehiculos");
--- src/vehiculo/VehiculoPalmaPachecoElisa2223.java
+++ src/vehiculo/VehiculoPalmaPachecoElisa2223.java
@@ -7,43 +7,59 @@ package vehiculo;
  */
 public class VehiculoPalmaPachecoElisa2223 {
 
-    private String nombre;
-    private double precio;
-    private double precioIVA;
-    private int stock;
+    private String nombre;//Nombre del vehículo
+    private double precio;//Precio del vehículo     
+    private double precioIVA;//IVA que obtendrá el precio del vehículo
+    private int stock;//Número de vehículos disponibles
 
-    /* Constructor sin argumentos */
+   /**
+     * Constructor sin parámetros.
+     */
     public VehiculoPalmaPachecoElisa2223 ()
     {
     }
-    // Constructor con parámetro para iniciar todas las propiedades de la clase
-    
-    
+    /**
+     * Constructor basado en el nombre del vehículo, el precio, así como en el stock.
+     * @param nom nombre del vehículo
+     * @param precio precio del vehículo
+     * @param stock cantidad de vehículos para vender
+     */
     public VehiculoPalmaPachecoElisa2223 (String nom, double precio, int stock)
     {
         this.nombre =nom;
         this.precio=precio;
         this.stock=stock;
     }
-   // Método para asignar el nombre del vehiculo
+   /**
+    * Método para asignar el nombre del vehiculo
+    * @param nom nombre del vehículo
+    */
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
-    // Método que me devuelve el nombre del vehiculo
+     /**
+     * Método que me devuelve el nombre del vehiculo
+     * @return Nombre
+     */
     public String obtenerNombre()
     {
         return getNombre();
     }
 
-    // Método que me devuelve el stock de vehiculos disponible en cada momento
+     /**
+     * Método que me devuelve el stock de vehiculos disponible en cada momento
+     * @return Stock
+     */
      public int obtenerStock ()
     {
         return getStock();
     }
 
-    /* Método para comprar vehiculos. Modifica el stock.
+    /**
+     * Método para comprar vehiculos. Modifica el stock.
      * Este método va a ser probado con Junit
+     * @param cantidad cantidad de vehículos que se van a comprar
      */
     public void comprar(int cantidad) throws Exception
     {
@@ -51,7 +67,11 @@ public class VehiculoPalmaPachecoElisa2223 {
             throw new Exception("No se puede comprar un nº negativo de vehiculos");
         setStock(getStock() + cantidad);
     }
-
+ /**
+      * Método para vender  vehiculos. Modifica el stock.
+     * Este método va a ser probado con Junit
+     * @param cantidad cantidad de vehículos que se van a vender
+     */
     public void vender (int cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -61,57 +81,65 @@ public class VehiculoPalmaPachecoElisa2223 {
         setStock(getStock() - cantidad);
     }
 
-    /**
-     * @return the nombre
+     /**
+     * Obtiene el nombre del vehículo
+     * @return nombre
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Asignar el nombre del vehículo
+     * @param nombre nombre que se va a asignar al vehículo
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the precio
+     * Obtiene el precio del vehículo
+     * @return precio
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @param precio the precio to set
+     *  Asignar el precio del vehículo
+     * @param precio precio que se va a asignar al vehículo
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the precioIVA
+     * Obtiene el precio IVA del vehículo
+     * @return  precioIVA
      */
     public double getPrecioIVA() {
         return precioIVA;
     }
 
     /**
-     * @param precioIVA the precioIVA to set
+     *  Asignar el precio IVA del vehículo
+     * @param precioIVA precio IVA  que se le va a asignar al vehículo
      */
     public void setPrecioIVA(double precioIVA) {
         this.precioIVA = precioIVA;
     }
 
     /**
-     * @return the stock
+     * Obtiene el stock de vehículos disponibles
+     * @return stock
      */
     public int getStock() {
         return stock;
     }
 
     /**
-     * @param stock the stock to set
+     * Asignar el stock de vehículos disponibles
+     * @param stock 
      */
     public void setStock(int stock) {
         this.stock = stock;
